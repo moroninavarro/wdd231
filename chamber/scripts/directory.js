7e77ad0c3c5d0324b44ff1959b3ae35be1466fd9
moronialanis <moronialanis23@gmail.com>
2025-05-17 00:44:19 -0700

    change

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,14 +1,19 @@
-async function getCompanieData(){
-    
-    const response = await fetch('../data/members.json');
-    const data = await response.json();
-    
-    displayCompanies(data.companies);
-    
-    
-} 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
 
-getCompanieData();
+gridbutton.addEventListener("click", () =>{
+    display.classList.add("#companies")
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList(){
+    display.classList.add("list")
+    display.classList.remove("#companies");
+}
+
+
 
 
 
@@ -21,13 +26,20 @@ const displayCompanies = (companies) =>{
         let address = document.createElement('p');
         let phoneNumber = document.createElement('p');
         let portrait = document.createElement('img');
+        let url = document.createElement('a');
+        let level = document.createElement('p');
         
+
+
         fullName.textContent = companie.name;
-        address.textContent = `${companie.address.street},${companie.address.city} `;
-        
-        phoneNumber.textContent = companie.phoneNumber;
+        address.textContent = `${companie.address.street}, ${companie.address.city}, ${companie.address.state}, ${companie.address.country}, ${companie.address.zip} `;
+        phoneNumber.textContent = `Phone number: ${companie.phoneNumber}`;
+        url.setAttribute('href', companie.url);
+        url.setAttribute('target', '_blank');
+        url.textContent = "Details";
+        level.textContent = `Membership Level: ${companie.membershipLevel}`;
         portrait.setAttribute('src', companie.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${companie.name} ${companie.address}`);
+        
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
@@ -35,6 +47,8 @@ const displayCompanies = (companies) =>{
         card.appendChild(fullName);
         card.appendChild(address);
         card.appendChild(phoneNumber);
+        card.appendChild(url);
+        card.appendChild(level);
         card.appendChild(portrait);
         cards.appendChild(card);
         
@@ -42,5 +56,16 @@ const displayCompanies = (companies) =>{
 }
 
 
+async function getCompanieData(){
+    
+    const response = await fetch('../chamber/data/members.json');
+    const data = await response.json();
+    
+    displayCompanies(data.companies);
+    
+    
+} 
+
+getCompanieData();
 
 
