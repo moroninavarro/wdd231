8f4f0a8cce6341e8de558e3f43c7cb735e5fa1be
moronialanis <moronialanis23@gmail.com>
2025-05-16 22:23:33 -0700

    change

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,11 +1,46 @@
 async function getCompanieData(){
     
-    const response = await fetch('members.json');
+    const response = await fetch('../data/members.json');
     const data = await response.json();
-    //console.table(data.prophets);
+    
     displayCompanies(data.companies);
     
     
 } 
+
 getCompanieData();
+
+
+
+const displayCompanies = (companies) =>{
+    const cards = document.querySelector('#cards');
+    companies.forEach((companie) =>{
+        
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let address = document.createElement('p');
+        let phoneNumber = document.createElement('p');
+        let portrait = document.createElement('img');
+        
+        fullName.textContent = companie.name;
+        address.textContent = `${companie.address.street},${companie.address.city} `;
+        
+        phoneNumber.textContent = companie.phoneNumber;
+        portrait.setAttribute('src', companie.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${companie.name} ${companie.address}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+        
+        card.appendChild(fullName);
+        card.appendChild(address);
+        card.appendChild(phoneNumber);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+        
+    });
+}
+
+
+
 
