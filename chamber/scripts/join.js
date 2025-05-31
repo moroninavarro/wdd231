fd87843d3e27e3378cc868c10f1193258a666c87
moronialanis <moronialanis23@gmail.com>
2025-05-30 21:28:45 -0700

    change

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,4 +1,18 @@
-function toggleDetails(id){
-    const details = document.getElementById(id);
-    details.classList.toggle("hidden");
+function openModal(texto){
+   const info = document.getElementById("modal-info");
+   const dialog = document.getElementById("theDetails");
+
+   info.textContent = texto;
+   dialog.showModal();
+
+   document.getElementById("closeModal").addEventListener("click", () =>{
+    dialog.close();
+});
+
 }
+
+
+
+
+
+
